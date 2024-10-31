ed658c3f93cf995a903eedc7fe0741b21e57a58c
maxim3638 <maximbortnik50@gmail.com>
2024-10-31 21:12:20 +0200

    Add files via upload

---
+++ коди.py
@@ -0,0 +1,13 @@
+name = "Maxim"
+surname = "Bortnik"
+age = 16
+hello = 'Hello world!'
+
+print(hello)
+print(name)
+print(surname)
+print(age)
+print(type(hello))
+print(type(name))
+print(type(surname))
+print(type(age))
